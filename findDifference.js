2b61f5739e04c0392319c5d8d340d45d2dc4a101
silpa4251 <silpadas4251@gmail.com>
2024-11-10 18:44:23 +0530

    leetcode

---
+++ findDifference.js
@@ -0,0 +1,9 @@
+var findTheDifference = function(s, t) {
+    let result =0;
+    for (let char of s + t) {
+        result ^= char.charCodeAt(0);
+    }
+    return String.fromCharCode(result);
+};
+
+console.log(findTheDifference("abcd","abcde"));
