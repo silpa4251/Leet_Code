37d1101f3cf0cb1f5ce55dcf15215312e4977a95
silpa4251 <silpadas4251@gmail.com>
2024-11-14 09:01:39 +0530

    leetcode

---
+++ countPrime.js
@@ -0,0 +1,15 @@
+var countPrimes = function(n) {
+    if(n<=2) return 0;
+    const isPrime = Array(n).fill(true);
+    isPrime[0]=isPrime[1]=false;
+    for(let i=2; i<n; i++){
+        if(isPrime[i]){
+            for(let j=i*i; j<n; j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+    return isPrime.reduce((count,num) => count+(num? 1 : 0),0);
+};
+
+console.log(countPrimes(50));
