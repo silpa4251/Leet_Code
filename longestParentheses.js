434785c3ff55bc12e4dfcd1f7afd92990bc729a7
silpa4251 <silpadas4251@gmail.com>
2024-11-20 10:21:08 +0530

    leetcode

---
+++ longestParentheses.js
@@ -0,0 +1,19 @@
+var longestValidParentheses = function(s) {
+    let maxLen = 0;
+    let stack =[-1];
+    for(let i=0;i < s.length;i++){
+        if(s[i] === '('){
+            stack.push(i);
+        } else {
+            stack.pop();
+            if(stack.length === 0){
+                stack.push(i);
+            } else {
+                maxLen = Math.max(maxLen, i-stack[stack.length-1])
+            }
+        }
+    }
+    return maxLen;
+};
+
+console.log(longestValidParentheses("(()()(())"));
