e72795b59b2759eff10fd9e466741668e4fbf01b
silpa4251 <silpadas4251@gmail.com>
2024-12-06 09:57:10 +0530

    leetcode

---
+++ validParenthesis.js
@@ -0,0 +1,21 @@
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{':'}',
+        '[': ']'
+    };
+    for(let char of s){
+        if(map[char]){
+            stack.push(map[char])
+        }
+        else{
+            if(stack.pop() !== char){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
+
+console.log(isValid('({}[]{})'));
