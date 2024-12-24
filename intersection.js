83f4d4620f3344914abe3f1048ffe52b4663a63e
silpa4251 <silpadas4251@gmail.com>
2024-12-24 09:38:45 +0530

    leetcode

---
+++ intersection.js
@@ -0,0 +1,9 @@
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const result = [...set1].filter(num => set2.has(num));
+    return result;
+
+};
+
+console.log(intersection([1,2,2,3],[2,2]));
