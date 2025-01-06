03b2889fd255831c739c5057741d2cb229e09b51
silpa4251 <silpadas4251@gmail.com>
2025-01-06 09:32:52 +0530

    leetcode

---
+++ strIncludes.js
@@ -0,0 +1,10 @@
+var numOfStrings = function(patterns, word) {
+    let count = 0;
+    for(const pattern of patterns){
+        if(word.includes(pattern)){
+            count ++;
+        }
+       
+    }
+     return count;
+};
