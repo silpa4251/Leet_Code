bb03db14bed5e7eaff5c8cc2ff6a2573ee30ea76
silpa4251 <silpadas4251@gmail.com>
2025-01-07 09:29:21 +0530

    leetcode

---
+++ valueOfVar.js
@@ -0,0 +1,12 @@
+var finalValueAfterOperations = function(operations) {
+    let res = 0;
+    for(let op of operations){
+        if(op === "--X" || op === "X--"){
+            res -=1;
+        }
+        else if (op === "++X" || op === "X++"){
+            res +=1;
+        }
+    }
+    return res;
+};
