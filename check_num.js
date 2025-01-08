293f19cc31bc37afbb15161feaaf72d887297ae2
silpa4251 <silpadas4251@gmail.com>
2025-01-08 09:46:22 +0530

    leetcode

--- check_num.js
+++ check_num.js
@@ -12,4 +12,4 @@ var areNumbersAscending = function(s) {
     }
     return true;
 };
-console.log(areNumbersAscending( "1 box has 3 blue 4 red 6 green and 12 yellow marbles"))
+console.log(areNumbersAscending("hello world 5 x 5"))
