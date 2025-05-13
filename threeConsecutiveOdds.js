42b140ffbd5dbdccec1342eef8fc5eb693926232
silpa4251 <silpadas4251@gmail.com>
2025-05-13 16:13:37 +0530

    three consecutive odds

--- threeConsecutiveOdds.js
+++ threeConsecutiveOdds.js
@@ -1,15 +1,12 @@
-var threeConsecutiveOdds = function(arr) {
-    let count = 0;
-    for(let i = 0; i<arr.length; i++){
-        if(arr[i]%2 !==0){
-         count++;
-        if(count === 3) return true;
-        }
-        else
-        count = 0;
-    }
-    return false;
+var threeConsecutiveOdds = function (arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      count++;
+      if (count === 3) return true;
+    } else count = 0;
+  }
+  return false;
 };
 
-console.log(threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]
-));
+console.log(threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]));
