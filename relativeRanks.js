99a71f0ac798b8c4e6cc54f7a9e69c4daa55c907
silpa4251 <silpadas4251@gmail.com>
2025-05-16 16:40:43 +0530

    Relative ranks

---
+++ relativeRanks.js
@@ -0,0 +1,20 @@
+var findRelativeRanks = function(score) {
+    const sorted = [...score].sort((a,b)=> b-a);
+    const ranks = new Map ();
+    sorted.forEach((val,index) => {
+        if(index === 0){
+            ranks.set(val,"Gold Medal");
+        } else if(index === 1){
+            ranks.set(val,"Silver Medal");
+        } else if(index === 2){
+            ranks.set(val,"Bronze Medal");
+        } else{
+            ranks.set(val,(index+1).toString());
+        }
+    });
+
+    return score.map(val => ranks.get(val));
+
+};
+
+console.log(findRelativeRanks([5,4,3,2,1]));
