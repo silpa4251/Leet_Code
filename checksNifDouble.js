d393e93ce2c176d996772e7aff0d70d24eb3208a
silpa4251 <silpadas4251@gmail.com>
2025-05-27 22:02:01 +0530

    checks if N has its double

--- checksNifDouble.js
+++ checksNifDouble.js
@@ -1,13 +1,13 @@
-function checkIfExist(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (i !== j && arr[i] === 2 * arr[j]) {
+var checkIfExist = function(arr) {
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length;j++){
+            if(i!== j && arr[j] === 2*arr[i]){
                 return true;
             }
         }
     }
     return false;
-}
+};
 
 const arr = [10, 2, 5, 3];
 console.log(checkIfExist(arr)); 
